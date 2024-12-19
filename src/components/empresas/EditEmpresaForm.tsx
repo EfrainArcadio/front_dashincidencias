d6a53462890a5166a1ba27993cfe614a7ed933dc
EfrainArcadio <efrain.arcadio@ort.cdmx.gob.mx>
2024-12-19 13:59:33 -0600

    updatesEmpresasForms

--- src/components/empresas/EditEmpresaForm.tsx
+++ src/components/empresas/EditEmpresaForm.tsx
@@ -1,5 +1,5 @@
 import EmpresaForm from "./EmpresaForm";
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Integrador, EmpresaFormData } from "@/types/index";
 import { useForm } from 'react-hook-form'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
@@ -45,24 +45,15 @@ export default function EditEmpresaForm({ data, empresaId }: EditIntegradorFormP
     <>
       <div className="max-w-3xl mx-auto space-y-6">
         <h1 className="text-5xl font-black text-gray-300 ">Editar {data.perfil}</h1>
-         <nav className="my-5 ">
-          <Link
-            className=" bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-            to='/'
-          >Volver a {data.perfil}es</Link>
-        </nav>
-
         <form
           className="mt-10 bg-white shadow-lg p-10 rounded-lg"
           onSubmit={handleSubmit(handleForm)}
           noValidate
         >
-
           <EmpresaForm
             register={register}
             errors={errors}
           />
-
           <input
             type="submit"
             value='Guardar Cambios'
