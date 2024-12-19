42c777caf0b6054ed266c08dbae59c5ca31b01f1
EfrainArcadio <efrain.arcadio@ort.cdmx.gob.mx>
2024-12-18 19:13:32 -0600

    NewUtil_isJUDCV

--- src/utils/policies.ts
+++ src/utils/policies.ts
@@ -1,5 +1,8 @@
-import {   Empresa, TeamMember, TypeAccount, UserA } from "../types"
+import {   Empresa, TeamMember, TypeAccount, User, UserA } from "../types"
+
+const adminEmail = import.meta.env.VITE_ADMIN_EMAIL;
 
 export const isManager = (managerId: Empresa['manager'], userId: TeamMember['_id']) => managerId === userId
 export const isCreador = (creadorId: Empresa['createdBy'], userId: TeamMember['_id']) => creadorId === userId
 export const TypeAccountVerify = (account: UserA['account'], admAccount: TypeAccount | undefined) => account === admAccount
+export const isJUDCV = (email: User['email']  ) => email === adminEmail
